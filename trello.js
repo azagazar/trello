9e4e2ec69337956e4f0523e761679b89b5fb4df2
Agata Zarod <Agata Zarod>
2017-04-20 16:21:59 +0200

    dodanie skryptu

---
+++ trello.js
@@ -0,0 +1,77 @@
+$(function() {
+
+	function randomString() {
+    var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
+    var str = '';
+    for (i = 0; i < 10; i++) {
+        str += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return str;
+}
+	function Column(name) {
+    var self = this; // przyda się dla funkcji zagnieżdżonych
+
+    this.id = randomString();
+    this.name = name;
+    this.$element = createColumn();
+
+    function createColumn() {
+		var $column = $('<div>').addClass('column');
+		var $columnTitle = $('<h2>').addClass('column-title').text(self.name);
+		var $columnCardList = $('<ul>').addClass('column-card-list');
+		var $columnDelete = $('<button>').addClass('btn-delete').text('x');
+		var $columnAddCard = $('<button>').addClass('add-card').text('Dodaj kartę');
+
+		$columnDelete.click(function() {
+        self.removeColumn();
+		});
+
+		$columnAddCard.click(function() {
+        self.addCard(new Card(prompt("Wpisz nazwę karty")));
+		});
+
+		$column.append($columnTitle)
+		        .append($columnDelete)
+		        .append($columnAddCard)
+		        .append($columnCardList);
+
+		return $column;
+    }
+    Column.prototype = {
+    addCard: function(card) {
+      this.$element.children('ul').append(card.$element);
+    },
+    removeColumn: function() {
+      this.$element.remove();
+    }
+	};
+  }
+
+function Card(description) {
+	var self = this;
+
+this.id = randomString();
+this.description = description;
+this.$element = createCard(); //
+
+function createCard() {
+var $card = $('<li>').addClass('card');
+var $cardDescription = $('<p>').addClass('card-description').text(self.description);
+var $cardDelete = $('<button>').addClass('btn-delete').text('x');
+$cardDelete.click(function(){
+        self.removeCard();
+});
+
+$card.append($cardDelete)
+	.append($cardDescription);
+return $card;
+}
+
+Card.prototype = {
+	removeCard: function() {
+		this.$element.remove();
+}
+}
+}
+
+})
